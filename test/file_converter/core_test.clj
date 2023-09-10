e7d39112ec6d6903933485310971e6d17c36ee22
MikeHardIce <mikehardice@gmail.com>
2023-09-10 13:36:22 +0100

    More generic tests

--- test/file_converter/core_test.clj
+++ test/file_converter/core_test.clj
@@ -8,12 +8,29 @@
   [extension files]
   (first (filter #(s/ends-with? (.getName %) extension) files)))
 
+(defn test-> [from-format to-format]
+  (let [label (str from-format "-" to-format)
+        folder (f/file (str "resources/test-cases/" label))]
+    (doseq [file-pair (group-by #(-> % .getName 
+                                     (s/replace from-format "")
+                                     (s/replace to-format "")) (rest (file-seq folder)))]
+      (let [[test-case files] file-pair
+            from (slurp (get-with-extension from-format files))
+            to (slurp (get-with-extension to-format files))]
+        (testing test-case
+          (is (= to (convert label from))))))))
+
 (deftest json->yaml-empty
   (testing "empty json"
     (is (= (convert "json-yaml" "{}")
            ""))))
 
-(deftest json->yaml-cases
+(deftest json->yaml
+  (test-> "json" "yaml"))
+
+(deftest edn->yaml
+  (test-> "edn" "yaml"))
+#_(deftest json->yaml-cases
   (let [folder (f/file "resources/test-cases/json-yaml")]
     (doseq [file-pair (group-by #(-> % .getName (s/replace #"(\.json|\.yaml)" "")) (rest (file-seq folder)))]
       (let [[test-case files] file-pair
